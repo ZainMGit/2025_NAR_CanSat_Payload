cfa7cbf870db5d0c42c2523fcc37b5ff528e8170
Zain Mustafa <mustafa98537@gmail.com>
2026-05-16 13:41:01 -0700

    Code Updates

--- Prototype_Program/Prototype_Program.ino
+++ Prototype_Program/Prototype_Program.ino
@@ -1,87 +1,133 @@
-#include <DHT11.h>
+#include <Wire.h>
 #include <SPI.h>
+#include <Adafruit_BMP280.h>
 #include <SD.h>
+#include <ISM330DHCXSensor.h>
+#include <SparkFun_MMC5983MA_Arduino_Library.h>
 
-// Pin definitions
-#define DHTPIN 3
-#define SD_CS 4   // Chip select pin for SD card
+//#define BMP_CS   (10)
+#define SD_CS    (10)
 
+Adafruit_BMP280 bmp;
+#define dev_interface Wire
+ISM330DHCXSensor AccGyr(&dev_interface);
+SFE_MMC5983MA mag;
 
-DHT11 dht11(DHTPIN);
+File dataFile, dataFile2, dataFile3;
+float manualTime = 0.00;
+int magZeroCount = 0;
 
-
-File myFile, myFile2; 
-
-const int DB_Sensor = A0;
-
-void setup() { 
+void setup() {
   Serial.begin(9600);
+  while (!Serial) delay(100);
 
-  // Wait for Serial (needed for some boards like Leonardo)
-  while (!Serial) {
-    ;
+  Serial.println(F("Initializing sensors and SD card..."));
+
+  if (!bmp.begin()) {
+    Serial.println(F("BMP280 not found!"));
+    while (1) delay(10);
   }
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
+                  Adafruit_BMP280::SAMPLING_X2,
+                  Adafruit_BMP280::SAMPLING_X16,
+                  Adafruit_BMP280::FILTER_X16,
+                  Adafruit_BMP280::STANDBY_MS_500);
 
-  Serial.print("Initializing SD card...");
+  dev_interface.begin();
+  AccGyr.begin();
+  AccGyr.ACC_Enable();
+  AccGyr.GYRO_Enable();
+
+  if (!mag.begin()) {
+    Serial.println(F("MMC5983MA not found!"));
+    while (1) delay(10);
+  }
 
   if (!SD.begin(SD_CS)) {
-    Serial.println("initialization failed!");
-    while (1);  // Stop here if SD fails
+    Serial.println(F("SD card init failed!"));
+    while (1) delay(10);
   }
-
-  Serial.println("initialization done.");
-
-  myFile = SD.open("test.txt", FILE_WRITE);
-
-  if (myFile) {
-    Serial.println("Writing to test.txt...");
-
-    myFile.println("This is a test file :)");
-    myFile.println("testing 1, 2, 3.");
-
-    for (int i = 0; i < 20; i++) {
-      myFile.println(i);
-    }
-
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // If the file didn't open, print an error
-    Serial.println("error opening test.txt");
-  }
+  Serial.println(F("Initialization done."));
 }
 
 void loop() {
-  int temperature = 0;
-  int humidity = 0;
+  float temperature = bmp.readTemperature();
+  float pressure = bmp.readPressure();
+  float altitude = bmp.readAltitude(1013);
 
-  // Read DHT11 data
-  int result = dht11.readTemperatureHumidity(temperature, humidity);
-
-  if (result == 0) {  // Successful read
-    Serial.print("Temp: ");
-    Serial.print(temperature);
-    Serial.print(" C\tHumidity: ");
-    Serial.print(humidity);
-    Serial.println(" %");
-  } else {
-    Serial.println("Failed to read from DHT11");
+  if (pressure < 30000 || pressure > 110000 || temperature < -40 || temperature > 85) {
+    Serial.println("Invalid BMP280 data, skipping loop.");
+    return;
   }
 
-  myFile = SD.open("test.txt", FILE_WRITE);
+  dataFile3 = SD.open("graphBMP.txt", FILE_WRITE);
+  if (dataFile3) {
+    dataFile3.print(manualTime);
+    dataFile3.print(" ");
+    dataFile3.print(pressure);
+    dataFile3.println();
+    dataFile3.flush();
+    dataFile3.close();
+  }
 
-  if (myFile) {
+  dataFile = SD.open("BMP280.txt", FILE_WRITE);
+  if (dataFile) {
+    dataFile.print("Temperature = "); dataFile.print(temperature); dataFile.println(" *C");
+    dataFile.print("Pressure = "); dataFile.print(pressure); dataFile.println(" Pa");
+    dataFile.print("Approx altitude = "); dataFile.print(altitude); dataFile.println(" m");
+    dataFile.println();
+    dataFile.close();
+  }
 
-    myFile.println("Starting Humidity readings");
-    myFile.println("Temp: ");
-    myFile.print(temperature);
-    Serial.println(" C\tHumidity: ");
-    Serial.print(humidity);
-    Serial.print(" %");
+  int32_t accelerometer[3];
+  int32_t gyroscope[3];
+  uint8_t accStatus = AccGyr.ACC_GetAxes(accelerometer);
+  uint8_t gyrStatus = AccGyr.GYRO_GetAxes(gyroscope);
 
-    myFile.close();
+  if (accStatus != 0 || gyrStatus != 0) {
+    Serial.println("Warning: IMU read failed. Reinitializing...");
+    AccGyr.end();
+    AccGyr.begin();
+    AccGyr.ACC_Enable();
+    AccGyr.GYRO_Enable();
+    return;
+  }
 
+  float magX = mag.getMeasurementX();
+  float magY = mag.getMeasurementY();
+  float magZ = mag.getMeasurementZ();
 
-  delay(1000);
-}
+  if (magX == 0.0 && magY == 0.0 && magZ == 0.0) {
+    magZeroCount++;
+    Serial.println("Warning: Magnetometer returned zeros.");
+    if (magZeroCount > 3) {
+      Serial.println("Magnetometer seems stuck. Reinitializing...");
+      mag.begin();
+      magZeroCount = 0;
+    }
+  } else {
+    magZeroCount = 0;
+  }
+
+  Serial.print("Acc[mg]: "); Serial.print(accelerometer[0]); Serial.print(", ");
+  Serial.print(accelerometer[1]); Serial.print(", "); Serial.print(accelerometer[2]);
+  Serial.print(" | Gyro[mdps]: "); Serial.print(gyroscope[0]); Serial.print(", ");
+  Serial.print(gyroscope[1]); Serial.print(", "); Serial.print(gyroscope[2]);
+  Serial.print(" | Mag[uT]: "); Serial.print(magX, 3); Serial.print(", ");
+  Serial.print(magY, 3); Serial.print(", "); Serial.println(magZ, 3);
+
+  dataFile2 = SD.open("position.txt", FILE_WRITE);
+  if (dataFile2) {
+    dataFile2.print("Acc[mg]: "); dataFile2.print(accelerometer[0]); dataFile2.print(", ");
+    dataFile2.print(accelerometer[1]); dataFile2.print(", "); dataFile2.print(accelerometer[2]);
+    dataFile2.print(" | Gyro[mdps]: "); dataFile2.print(gyroscope[0]); dataFile2.print(", ");
+    dataFile2.print(gyroscope[1]); dataFile2.print(", "); dataFile2.print(gyroscope[2]);
+    dataFile2.print(" | Mag[uT]: "); dataFile2.print(magX, 3); dataFile2.print(", ");
+    dataFile2.print(magY, 3); dataFile2.print(", "); dataFile2.println(magZ, 3);
+    dataFile2.println();
+    dataFile2.close();
+  }
+
+  manualTime += 2;
+  delay(1800);
 }
